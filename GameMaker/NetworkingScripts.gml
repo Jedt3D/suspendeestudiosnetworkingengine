23c2ce8e0e3616d64c2aabafb59b823983642dcc
SuspendeeStudios <suspendeestudios@gmail.com>
2017-01-26 10:33:56 -0500

    Added Python Server and GameMaker Files
    Plans - Adding GM:S2 Support

---
+++ GameMaker/NetworkingScripts.gml
@@ -0,0 +1,87 @@
+#define network_process_packet
+//Splitter is this ~§~
+//Good example "Ok~§~banana~§~one~§~"
+if (ds_map_find_value(async_load,'type') == network_type_data)
+{
+var buffer = ds_map_find_value(async_load,'buffer');
+str = buffer_read(ds_map_find_value(async_load,'buffer'),buffer_text);
+
+map = ds_map_create();
+//show_message(str)
+while (string_pos("~§~",str) != 0)
+{
+
+var susdat = json_decode( string_copy(str,0,string_pos("~§~",str)-1));
+
+    for (i=0;i<ds_list_size(global.listenerlist);i++)
+    {  var listenerlist = global.listenerlist[| i];
+        if (susdat[? "path"] == listenerlist && ds_map_exists(susdat,listenerlist))
+       { 
+       
+        script_execute( ds_map_find_value(global.listener, susdat[? "path"])
+        ,susdat[? "path"],ds_map_find_value( susdat,listenerlist) 
+        )
+        }
+       
+    }
+ds_map_destroy(susdat);
+str = string_delete(str,1,string_pos("~§~",str)+2)
+}
+}
+
+#define network_emit
+// Network_Emit(Path,String)
+//This will send either a raw JSON value or a raw text value of your choice automatically
+var suspath = argument0;
+var sustxt = argument1;
+
+var susbuf = buffer_create(1024,buffer_grow,1);
+buffer_seek(susbuf,buffer_seek_start,0);
+var susmap = ds_map_create();
+
+if (ds_exists(sustxt,ds_type_map))
+{
+ds_map_add_map(susmap,suspath,sustxt);
+}
+else
+{
+ds_map_add(susmap,suspath,sustxt);
+}
+
+
+buffer_write(susbuf,buffer_text,json_encode(susmap)+global.splitter);
+ds_map_destroy(susmap);
+network_send_raw(global.socket,susbuf,buffer_tell(susbuf));
+buffer_delete(susbuf);
+
+#define network_listen
+//network_listen(path,script)
+// In script do var name = argument0 and var data = argument1
+//
+var path = argument0;
+var script = argument1;
+
+ds_map_add(global.listener,path,script);
+ds_list_add(global.listenerlist,path);
+
+#define quick_json
+var susbuf = ds_map_create();
+ds_map_add(susbuf,argument0,argument1);
+return susbuf;
+ds_map_destroy(susbuf);
+
+#define network_connect_tcp
+global.listener = ds_map_create();
+global.listenerlist = ds_list_create();
+global.socket = network_create_socket(network_socket_tcp);
+global.server = network_connect_raw(global.socket,argument0,argument1);
+global.splitter = "~§~"
+
+#define handledata_example
+var name = argument0;
+var data = argument1;
+
+show_message("DATA RECEIVED");
+show_message(data);
+show_message(name);
+
---
+++ Server/gmserver.py
@@ -0,0 +1,153 @@
+import socket
+import json
+import threading
+import os
+#Storage and Lists to keep track of users
+userstorage = {}
+userlist = []
+userasync = {}
+#Splitter, indicates the end of the message
+splitter = "~§~"
+#List of functions to use for each user
+funclist = []
+messlist = []
+
+
+#Example
+def banana(message,client):
+    global splitter
+    print("Printing what was in the path")
+    jso = json.loads(message)
+    print(jso['data'])
+    print('Banana is activated!')
+    #Note: If you send from the client the number as a string it will stay as a astirng until you convert it.
+    emit("event",str(int(jso['data'])*2),client)
+
+def addFunc(message,messfunc):
+    #Add functions
+    #Use: addFunc("<event>","function (for example playerData() would be playerData)")
+    global funclist
+    global messlist
+    funclist.append(messfunc)
+    messlist.append(message)
+    print(funclist)
+
+
+
+def useFunc():
+    #Uses all functions
+    while True:
+        global funclist
+        global userlist
+        global messlist
+        global userasync
+        for f in funclist:
+            for m in messlist:
+                for c in userlist:
+                    if str(c) in userasync:
+                        if m in userasync[str(c)]:
+                            if userasync[str(c)][m] != []:
+                                if messlist.index(m) == funclist.index(f):
+                                    useData(m,f,c)
+
+
+def useData(message,func,client):
+    global userasync
+    if str(client) in userasync:
+        if message in userasync[str(client)]:
+            for i in userasync[str(client)][message]:
+                func(i,client)
+            userasync[str(client)][message] = []
+
+def fetchdata(message,client):
+    global userasync
+    print(userasync)
+    if message in userasync[str(client)]:
+        return userasync[str(client)][message]
+    else:
+        return None
+def processdata(message,client):
+    global userasync
+    global splitter
+    try:
+        meslis = message.split("~§~")
+        meslis.remove('')
+        print("Splitting it into a list for you sir.")
+        for i in meslis:
+            mesdat = json.loads(i)
+            for key in list(mesdat.keys()):
+                if not key in userasync[str(client)]:
+                        userasync[str(client)][key] = []
+                        #JSON.DUMPS converts keys back into strings, you may remove this if you wish
+                        userasync[str(client)][key].append(json.dumps(mesdat[key]))
+    except:
+        #Protects thread from crashing to baddies
+        pass
+
+
+
+def emit(path,message,client):
+    #Message must be JSON or Message, and must be used in handleclient
+    global splitter
+
+    #
+    emitdata = {}
+    emitdata[path] = message
+    emitdata["path"] = path
+    try:
+        client.send((json.dumps(emitdata)+splitter).encode('utf-8'))
+        return True
+    except:
+        return False
+def handleclient(client,addr):
+    global splitter
+    global userasync
+    userasync[str(client)] = {}
+    while True:
+        sent = emit("event","{'msgid':0}",client)
+        if not sent:
+            global userlist
+            print("Client from the IP Address {} has disconnected".format(str(addr[0])))
+            userlist.remove(client)
+            break;
+        data = ""
+        useData("authentication",banana,client)
+        try:
+            data = client.recv(1024)
+            processdata(data.decode('utf-8'),client)
+        except:
+            pass
+        if not data:
+            global userlist
+            print("Client from the IP Address {} has disconnected".format(str(addr[0])))
+            userlist.remove(client)
+            break;
+        #print(data)
+        datv = fetchdata("authentication",client)
+        #print(json.loads(datv[0]))
+
+
+
+def server(ip,port,connlimit):
+    s = socket.socket()
+    s.bind((ip,port))
+    s.listen(connlimit)
+    global userstorage
+    global userlist
+    global userasync
+    addFunc("authentication",banana)
+    threading.Thread(target=useFunc,args=[]).start()
+    while True:
+        c, addr = s.accept()
+        userstorage[str(c)] = True
+        userlist.append(c)
+        threading.Thread(target=handleclient,args=[c,addr]).start()
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("Server Has Started")
+    server('0.0.0.0',14579,20)
