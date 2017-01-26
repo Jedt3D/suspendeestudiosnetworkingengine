5b7f158a95aa2ac28ef0612e929880166ab8ed4a
SuspendeeStudios <suspendeestudios@gmail.com>
2017-01-26 11:37:14 -0500

    Removed printing functions

--- Server/testserver.py
+++ Server/testserver.py
@@ -1,5 +1,6 @@
 import gmserver
-
+import json
+iden = 0
 
 
 app = gmserver
@@ -8,6 +9,17 @@ def cow(message,client):
     app.emit("event","I am a banana",client)
     print('sent data')
 
-app.addFunc("event",cow)
+def auth(message,client):
+    global iden
+    auth = json.loads(message)
 
+    setup = {'id':iden,'x':500,'y':500,'msgid':5}
+    print("Sending Data")
+    app.emit("event",json.dumps(setup),client)
+    print("Sending Data")
+    iden = iden + 1
+
+
+#app.addFunc("event",cow)
+app.addFunc("authentication",auth)
 app.server('0.0.0.0',14579,20)
